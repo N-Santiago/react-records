313d602551c32a23fe37028f894f75393218ac0b
N-Santiago <norbertosantiago82@gmail.com>
2021-06-14 18:41:04 -0500

    routes

--- react-records-backend/config/routes.rb
+++ react-records-backend/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :albums, only: [:index, :show, :create, :destroy]
-  resources :categories, only: [:index]
+  # resources :albums, only: [:index, :show, :create, :destroy]
+  # resources :categories, only: [:index]
+  resources :categories do
+    resources :albums
+  end 
+  resources :albums
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
