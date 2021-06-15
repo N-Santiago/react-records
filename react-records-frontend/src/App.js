21ddb1fed71ca360bfe7e8c5d7bbc4d2ffaaa132
N-Santiago <norbertosantiago82@gmail.com>
2021-06-14 20:49:29 -0500

    store and imports on index file

--- react-records-frontend/src/App.js
+++ react-records-frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import CategoriesContainer from './containers/CategoriesContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {/* Learn React App */}
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>React Records</h1>
+        {/* < CategoriesContainer /> */}
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ react-records-frontend/src/store.js
@@ -0,0 +1,15 @@
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux'
+import thunk from 'redux-thunk'
+// import categories from './reducers/categoryReducer'
+
+const reducer = combineReducers({
+    // categories,
+    // albums
+})
+
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+export default store
